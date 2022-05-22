defec33b4e284147101b194d357cfd888f7f4b87
Sara Salazar <sara.salazarsdad@gmail.com>
2022-05-22 17:44:05 -0400

    Acabdo de estructuras de totales

--- routes/Report.js
+++ routes/Report.js
@@ -1,13 +1,67 @@
 const express = require("express");
 const router = express.Router();
-const { Provider, Sales } = require("../config/config_test");
+const { Provider, Sale,Product,User,Coupon,Customer} = require("../config/config_test");
 
-// router.get("/", async (req, res) => {
-//   const snapshot = await Customer.get();
-//   const list = snapshot.collection.lenght;
-//   res.send('hello');
-//   console.log('cantidad: ', list)
-// });
+router.get("/provider", async (req, res) => {
+    try {
+    const snapshot = await Provider.get();
+    const total= snapshot.size;
+    res.send({total});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
+router.get("/product", async (req, res) => {
+    try {
+    const snapshot = await Product.get();
+    const totalp= snapshot.size;
+    res.send({totalp});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
+router.get("/user", async (req, res) => {
+    try {
+    const snapshot = await User.get();
+    const total= snapshot.size;
+    res.send({total});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
+router.get("/coupon", async (req, res) => {
+    try {
+    const snapshot = await Coupon.get();
+    const total= snapshot.size;
+    res.send({total});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
+router.get("/customer", async (req, res) => {
+    try {
+    const snapshot = await Customer.get();
+    const total= snapshot.size;
+    res.send({total});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
+router.get("/sales", async (req, res) => {
+    try {
+    const snapshot = await Sale.get();
+    const total= snapshot.size;
+    res.send({total});
+    } catch (error) {
+      console.log(error)  
+    }
+  
+});
 
 // router.get("/provider", async (req, res) => {
 //   const snapshot = await Provider.get();
