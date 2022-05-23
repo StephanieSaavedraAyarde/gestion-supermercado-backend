d8046eb41b0f76c7864d406b152b3dbb333c0522
Stephanie <stephanie.saavedra@ucb.edu.bo>
2022-05-22 22:19:16 -0400

    Dasboard Data

--- routes/Report.js
+++ routes/Report.js
@@ -3,84 +3,80 @@ const router = express.Router();
 const { Provider, Sale, Product, User, Coupon, Customer } = require("../config/config_test");
 
 router.get("/provider", async (req, res) => {
-    try {
-        const snapshot = await Provider.get();
-        const total = snapshot.size;
-        res.send({ total });
-    } catch (error) {
-        console.log(error)
-    }
-
+  try {
+    const snapshot = await Provider.get();
+    const total = snapshot.size;
+    res.send({ total });
+  } 
+  catch(error){
+    console.log(error);
+  }
 });
 
 router.get("/product", async (req, res) => {
-    try {
-        const snapshot = await Product.get();
-        const totalp = snapshot.size;
-        res.send({ totalp });
-    } catch (error) {
-        console.log(error)
-    }
-
+  try{
+    const snapshot = await Product.get();
+    const totalp = snapshot.size;
+    res.send({ totalp });
+  }
+  catch(error){
+    console.log(error);
+  }
 });
 
 router.get("/user", async (req, res) => {
-    try {
-        const snapshot = await User.get();
-        const total = snapshot.size;
-        res.send({ total });
-    } catch (error) {
-        console.log(error)
-    }
-
+  try {
+    const snapshot = await User.get();
+    const total = snapshot.size;
+    res.send({ total });
+  } 
+  catch(error){
+    console.log(error);
+  }
 });
 
 router.get("/coupon", async (req, res) => {
-    try {
-        const snapshot = await Coupon.get();
-        const total = snapshot.size;
-        res.send({ total });
-    } catch (error) {
-        console.log(error)
-    }
-
+  try {
+    const snapshot = await Coupon.get();
+    const total = snapshot.size;
+    res.send({ total });
+  } 
+  catch(error){
+      console.log(error);
+  }
 });
 
 router.get("/customer", async (req, res) => {
-    try {
-        const snapshot = await Customer.get();
-        const total = snapshot.size;
-        res.send({ total });
-    } catch (error) {
-        console.log(error)
-    }
-
-});
-router.get("/sales", async (req, res) => {
-    try {
-        const snapshot = await Sale.get();
-        const total = snapshot.size;
-        res.send({ total });
-    } catch (error) {
-        console.log(error)
-    }
-
-});
-router.get("/total", async (req, res) => {
-
-
-    const querySnapshot = await Sale.get();
-    const documents = querySnapshot.docs;
-
-    let total = 0;
-    for (const doc of documents) {
-       
-        total += doc.get('total');
-    }
+  try {
+    const snapshot = await Customer.get();
+    const total = snapshot.size;
     res.send({ total });
-    console.log("TOTAL"+total) ;
+  } 
+  catch(error){
+      console.log(error);
+  }
+});
 
+router.get("/sales", async (req, res) => {
+  try {
+    const snapshot = await Sale.get();
+    const total = snapshot.size;
+    res.send({ total });
+  } 
+  catch(error){
+      console.log(error);
+  }
+});
 
+router.get("/total", async (req, res) => {
+  const querySnapshot = await Sale.get();
+  const documents = querySnapshot.docs;
+  let total = 0;
+  for (const doc of documents) {
+      total += doc.get('total');
+  }
+  res.send({ total });
+  console.log("TOTAL"+total);
 });
 
 module.exports = router;
