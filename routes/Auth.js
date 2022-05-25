59fc054d75a8bd001447a538df57d0d3af5d20e3
Ignacio Paz <yamil.paz@ucb.edu.bo>
2022-05-25 08:50:18 -0400

    Added token verification

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { Auth } = require("../config/config_test");
+const { admin } = require("../config/config_admin");
+const jwt = require("jsonwebtoken");
+
 
 //LogIn
 router.post("/login", async (req, res) => {
@@ -20,6 +23,30 @@ router.post("/login", async (req, res) => {
 });
 
 //VerificarToken
-router.post("/token", async (req, res) => {});
+router.post("/token", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  let idToken = data.token;
+
+  admin.auth().verifyIdToken(idToken)
+    .then((decodedIdToken) => {
+      console.log("ID Token correctly decoded", decodedIdToken);
+      admin
+        .auth()
+        .getUser(decodedIdToken.uid)
+        .then((userRecord) => {
+          return res.send(userRecord);
+        })
+        .catch((error) => {
+          console.error("Error while getting Firebase User record:", error);
+          return res.send({ code: 403, error: "Unauthorized" });
+        });
+    })
+    .catch((error) => {
+      console.error("Error while verifying Firebase ID token:", error);
+      return res.send({ code: 403, error: "Unauthorized" });
+    });
+});
 
 module.exports = router;
